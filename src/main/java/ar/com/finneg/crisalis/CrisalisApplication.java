4275fdefd2f8d1bf8f43a4d23087acb1689feb7d
Duarte,Andres <duarteandresj@gmail.com>
2022-12-27 22:57:21 -0300

    Primer Commit

---
+++ src/main/java/ar/com/finneg/crisalis/CrisalisApplication.java
@@ -0,0 +1,13 @@
+package ar.com.finneg.crisalis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrisalisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrisalisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/finneg/crisalis/CrisalisApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.finneg.crisalis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrisalisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
